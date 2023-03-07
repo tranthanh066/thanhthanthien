e8d6b0dbd32c970733008aae7bf0d78ed900e269
Thaanh <tranhoangthanh666@gmail.com>
2023-03-08 00:36:15 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Good morning and Happy Woman"s Day',
+    introDesc: `Chúc Tiên có một ngày Quốc tế con gái vui vẻ và ngày càng xink đẹp học dỏi nhé`,
+    btnIntro: 'OK!!',
+    title: 'Cậu thấy tớ cúp học Thân có đáng không:))',
+    desc: 'Ngày quan trọng nên mới nàm dậy:> ',
+    btnYes: 'Có, hợp lí vải ò 💖💖💖',
+    btnNo: 'Không nhé  ',
+    question:'Sau tớ cúp cậu chụp bài cho tớ nhé😊😊',
+    reasonPlaceholder: 'Trả lời',
+    btnReply: 'Send ! 💌💌',
+    reply: 'Tất nhiên là vậy rồi😊😊',
+    mess: 'Kảm ơn cậu nhiều hê hê:)',
+    messDesc: '💬💬Lần nữa chúc cậu 8/3 hoan hỉ nhó và Chộp bài cho tớ nữa nha💬💬',
+    btnAccept: 'Ok ãnh 💖',
+    messLink: 'https://m.me/PhungQuocDinh'
+}
